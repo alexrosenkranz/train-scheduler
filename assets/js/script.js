ed4b25d10ce7f76958fb12dc1235048b879f3fec
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2017-01-18 15:12:55 -0500

    created edit functunctions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 
   var dataRef = firebase.database();
   var currentTime = moment();
+  var editTrainKey = '';
   // var nextTrain = '';
   // var trainOnDeck = '';
   // var timer = '';
@@ -38,15 +39,27 @@ $(document).ready(function(){
     $('#trainDestination').val('');
     $('#firstTrain').val('');
     $('#trainFrequency').val('');
+    $('#trainKey').val('');
+
 
     // Push to firebase
-    dataRef.ref().child('trains').push({
-      trainName: trainName,
-      trainDestination: trainDestination,
-      trainTime: trainTime,
-      trainFreq: trainFreq,
+    if (editTrainKey == ''){ 
+      dataRef.ref().child('trains').push({
+        trainName: trainName,
+        trainDestination: trainDestination,
+        trainTime: trainTime,
+        trainFreq: trainFreq,
+      })
+    } else if (editTrainKey != '') {
+      dataRef.ref('trains/' + editTrainKey).update({
+        trainName: trainName,
+        trainDestination: trainDestination,
+        trainTime: trainTime,
+        trainFreq: trainFreq,
+      })
+      editTrainKey = '';
+    }
 
-    })
   });
 
 
@@ -74,23 +87,66 @@ $(document).ready(function(){
       timeDiffTotal = Math.abs(timeDiff - 1);
     }
 
-    console.log(newTime);
-
     $('tbody').append("<tr class=" + trainClass + "><td>" + trainId.trainName + "</td><td>" +
       trainId.trainDestination + "</td><td>" + 
       trainId.trainFreq + "</td><td>" +
       newTime + "</td><td>" +
-      timeDiffTotal + "</td><td><button type='button' class='delete' data-train=" + trainClass + ">Remove</button></td></tr>");
+      timeDiffTotal + "</td><td><button type='button' class='edit' data-train=" + trainClass + ">Edit</button><button type='button' class='delete' data-train=" + trainClass + ">Remove</button></td></tr>");
+  }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+  });
+
+   // Reference Firebase when page loads to check for stored information 
+  dataRef.ref().child('trains').on('child_changed', function(childSnapshot){
+    
+    console.log(childSnapshot.val());
+    var trainClass = childSnapshot.key;
+    var trainId = childSnapshot.val();
+
+    var firstTimeConverted = moment.unix(trainId.trainTime);
+    var timeDiff = moment().diff(moment(firstTimeConverted, 'HH:mm'), 'minutes');
+
+    var timeDiffCalc = timeDiff % parseInt(trainId.trainFreq);
+    var timeDiffTotal = parseInt(trainId.trainFreq) - timeDiffCalc;
+
+    var newTime;
+
+    if(timeDiff > 0) {
+      newTime = currentTime.add(timeDiffTotal, 'minutes').format('hh:mm A');
+    } else {
+      newTime = firstTimeConverted.format('hh:mm A');
+      timeDiffTotal = Math.abs(timeDiff - 1);
+    }
+
+    $('.'+trainClass).html("<td>" + trainId.trainName + "</td><td>" +
+      trainId.trainDestination + "</td><td>" + 
+      trainId.trainFreq + "</td><td>" +
+      newTime + "</td><td>" +
+      timeDiffTotal + "</td><td><button type='button' class='edit' data-train=" + trainClass + ">Edit</button><button type='button' class='delete' data-train=" + trainClass + ">Remove</button></td>");
   }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
   });
 
 
 
-    $(document).on('click','.delete', function(){
-      var trainKey = $(this).attr('data-train');
-      dataRef.ref("trains/" + trainKey).remove();
-      $('.'+ trainKey).remove();
+
+  $(document).on('click','.delete', function(){
+    var trainKey = $(this).attr('data-train');
+    dataRef.ref("trains/" + trainKey).remove();
+    $('.'+ trainKey).remove();
+  });
+
+  $(document).on('click','.edit', function(){
+    editTrainKey = $(this).attr('data-train');
+    dataRef.ref("trains/" + editTrainKey).once('value').then(function(childSnapshot) {
+      $('#trainName').val(childSnapshot.val().trainName);
+      $('#trainDestination').val(childSnapshot.val().trainDestination);
+      $('#firstTrain').val(moment.unix(childSnapshot.val().trainTime).format('HH:mm'));
+      $('#trainFrequency').val(childSnapshot.val().trainFreq);
+      $('#trainKey').val(childSnapshot.key);
+
     })
+    
+  })
 
 });
