e913df82c734f9a18a870d1197a73c3cb4cc30df
alex@netwaveinteractive.com <alex.rosenkranz@gmail.com>
2017-01-19 17:48:13 -0500

    fixed authentication

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,40 +9,42 @@ $(document).ready(function(){
     messagingSenderId: "167602757568"
   };
   firebase.initializeApp(config);
-  var provider = new firebase.auth.GoogleAuthProvider();
+  
 
-  $(document).on('click', '.g-signin2', function() {
-    firebase.auth().signInWithRedirect(provider);
+$('.content').hide();
+// Using a popup.
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
+
+$(document).on('click', '.signIn', function() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+   // This gives you a Google Access Token.
+   var token = result.credential.accessToken;
+   // The signed-in user info.
+   var user = result.user;
+   $('.content').show();
+   
   });
-  firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
+  $(this).removeClass('signIn')
+    .addClass('signOut')
+    .html('Sign Out Of Google');
 });
- var user = firebase.auth().currentUser;
 
-if (user != null) {
-  user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: "+profile.providerId);
-    console.log("  Provider-specific UID: "+profile.uid);
-    console.log("  Name: "+profile.displayName);
-    console.log("  Email: "+profile.email);
-    console.log("  Photo URL: "+profile.photoURL);
+$(document).on('click', '.signOut', function () {
+  firebase.auth().signOut().then(function() {
+    $('.content').hide();
+  }, function(error) {
+    // An error happened.
   });
-}
+  $(this).removeClass('signOut')
+    .addClass('signIn')
+    .html('Sign In With Google');
+});
+
+
+
+
 
   // Declare variables
 
