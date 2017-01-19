4a3d7cb998a297d187059d05f43c07c721293362
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2017-01-19 15:46:59 -0500

    google auth redirect

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,53 +10,28 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
-  // Firebase Authentication
-  function onSignIn(googleUser) {
-    console.log('Google Auth Response', googleUser);
-
-    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
-    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-      unsubscribe();
-      // Check if we are already signed-in Firebase with the correct user.
-      if (!isUserEqual(googleUser, firebaseUser)) {
-        // Build Firebase credential with the Google ID token.
-        var credential = firebase.auth.GoogleAuthProvider.credential(
-            googleUser.getAuthResponse().id_token);
-
-        // Sign in with credential from the Google user.
-        firebase.auth().signInWithCredential(credential).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-        });
-
-      } else {
-        console.log('User already signed-in Firebase.');
-      }
-    });
+  $(document).on('click', '.g-signin2', function() {
+    firebase.auth().signInWithRedirect(provider);
   }
-
-  function isUserEqual(googleUser, firebaseUser) {
-  if (firebaseUser) {
-    var providerData = firebaseUser.providerData;
-    for (var i = 0; i < providerData.length; i++) {
-      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === googleUser.getBasicProfile().getId()) {
-        // We don't need to reauth the Firebase connection.
-        return true;
-      }
-    }
+  firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
   }
-  return false;
-}
-
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
  var user = firebase.auth().currentUser;
- console.log(user);
 
 if (user != null) {
   user.providerData.forEach(function (profile) {
