01133a3534488a1f5c9bfb0811bc406d3b067279
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2017-02-02 14:21:17 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Train Scheduler
 Week 7 Homework - Train Schedule with Firebase
 
-# Features
+[View Project.] (https://floating-cliffs-89171.herokuapp.com/)
+
+## Features
 - Ability to add, edit, or remove a train to and from the schedule
 - Timetable auto updates
-- Must log in with a Google account
+- Must log in with a Google account to view train information
+
+## Technologies
+- HTML
+- Twitter Bootstrap
+- JavaScript
+- jQuery
+- Firebase
--- assets/js/script.js
+++ assets/js/script.js
@@ -123,31 +123,30 @@ $(document).ready(function(){
     // Reference Firebase when page loads and train added
     dataRef.ref().child('trains').on('value', function(snapshot){
       $('tbody').empty();
+      
       snapshot.forEach(function(childSnapshot){
+        var trainClass = childSnapshot.key;
+        var trainId = childSnapshot.val();
+        var firstTimeConverted = moment.unix(trainId.trainTime);
+        var timeDiff = moment().diff(moment(firstTimeConverted, 'HH:mm'), 'minutes');
+        var timeDiffCalc = timeDiff % parseInt(trainId.trainFreq);
+        var timeDiffTotal = parseInt(trainId.trainFreq) - timeDiffCalc;
 
+        if(timeDiff >= 0) {
+          newTime = null;
+          newTime = moment().add(timeDiffTotal, 'minutes').format('hh:mm A');
 
-      var trainClass = childSnapshot.key;
-      var trainId = childSnapshot.val();
-      var firstTimeConverted = moment.unix(trainId.trainTime);
-      var timeDiff = moment().diff(moment(firstTimeConverted, 'HH:mm'), 'minutes');
-      var timeDiffCalc = timeDiff % parseInt(trainId.trainFreq);
-      var timeDiffTotal = parseInt(trainId.trainFreq) - timeDiffCalc;
+        } else {
+          newTime = null;
+          newTime = firstTimeConverted.format('hh:mm A');
+          timeDiffTotal = Math.abs(timeDiff - 1);
+        }
 
-      if(timeDiff >= 0) {
-        newTime = null;
-        newTime = moment().add(timeDiffTotal, 'minutes').format('hh:mm A');
-
-      } else {
-        newTime = null;
-        newTime = firstTimeConverted.format('hh:mm A');
-        timeDiffTotal = Math.abs(timeDiff - 1);
-      }
-
-      $('tbody').append("<tr class=" + trainClass + "><td>" + trainId.trainName + "</td><td>" +
-        trainId.trainDestination + "</td><td>" + 
-        trainId.trainFreq + "</td><td>" +
-        newTime + "</td><td>" +
-        timeDiffTotal + "</td><td><button class='edit btn' data-train=" + trainClass + "><i class='glyphicon glyphicon-pencil'></i></button> <button class='delete btn' data-train=" + trainClass + "><i class='glyphicon glyphicon-remove'></i></button></td></tr>");
+        $('tbody').append("<tr class=" + trainClass + "><td>" + trainId.trainName + "</td><td>" +
+          trainId.trainDestination + "</td><td>" + 
+          trainId.trainFreq + "</td><td>" +
+          newTime + "</td><td>" +
+          timeDiffTotal + "</td><td><button class='edit btn' data-train=" + trainClass + "><i class='glyphicon glyphicon-pencil'></i></button> <button class='delete btn' data-train=" + trainClass + "><i class='glyphicon glyphicon-remove'></i></button></td></tr>");
 
     });
     }, function(errorObject) {
